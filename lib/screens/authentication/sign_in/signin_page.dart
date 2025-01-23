7a22f30e3cd2b29ee26e65e371c847a5e85a5b99
redietkuku <redietatsede@gmail.com>
2025-01-24 02:56:56 +0300

    added sign social media buttons, separator and email and password textfields

--- lib/screens/authentication/sign_in/signin_page.dart
+++ lib/screens/authentication/sign_in/signin_page.dart
@@ -15,19 +15,22 @@ class _SignInPageState extends State<SignInPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        
+        child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
 
         children: [
-          const SizedBox(height: 5), // spacing from the top
+          const SizedBox(height: 2), // spacing from the top
           Image.asset(
             'assets/images/linkedin_offical_logo.png',
             width: 100.0,
             height: 100.0,
             ),
           
-          const SizedBox(height: 15),
+          const SizedBox(height: 10),
           Text(
             'Sign in',
             style: TextStyle(
@@ -50,7 +53,7 @@ class _SignInPageState extends State<SignInPage>{
             TextSpan(
               text: 'Join Linkedin',
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 16,
                 color: Colors.blue,
                 fontWeight: FontWeight.bold,
               )
@@ -60,8 +63,254 @@ class _SignInPageState extends State<SignInPage>{
           ],
             ),
           ),
+
+          const SizedBox(height:15),
+          // Butttons
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: OutlinedButton(
+            onPressed: (){
+              print('Sign in with Google clicked');
+          }, 
+          style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Colors.black, width: 1) ,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20), //rounded corners
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 10), //button padding
+
+          ),
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              Image.asset(
+                'assets/images/google_icon.png',
+                width: 30,
+                height: 30,
+              ),
+              SizedBox(width: 8),
+              Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+
+                )
+              ),
+            ],
+          )
+          )
+          ),
+
+          const SizedBox(height:12),
+
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            child:
+          OutlinedButton(
+            onPressed: (){
+              print('Sign in with Google clicked');
+          }, 
+          style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Colors.black, width: 1) ,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20), //rounded corners
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 10), //button padding
+
+          ),
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              Image.asset(
+                'assets/images/apple_icon.png',
+                width: 30,
+                height: 30,
+              ),
+              SizedBox(width: 8),
+              Text(
+                'Sign in with Apple',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+
+                )
+              ),
+            ],
+          )
+          )
+          ),
+
+          const SizedBox(height:12),
+
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: OutlinedButton(
+            onPressed: (){
+              print('Sign in Facebook clicked');
+            },
+            style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Colors.black, width: 1) ,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20), //rounded corners
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 10), //button padding
+
+          ),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              Image.asset(
+                'assets/images/facebook_icon.png',
+                width: 30,
+                height: 30,
+              ),
+              SizedBox(width: 8),
+              Text(
+                'Sign in with Facebook',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+
+                )
+              ),
+            ],
+          ) 
+            ),
+          ),
+
+          const SizedBox(height:15),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Divider(
+                  thickness: 0.5,
+                  color: Colors.grey,
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'or',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                )
+              ),
+
+              Expanded(
+                child: Divider(
+                  thickness: 0.5,
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          )
+          ),
+
+          const SizedBox(height:15),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child:
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Email Text Form Field
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email or Phone',
+                  hintText: 'Email or Phone',
+                  border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                  ),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                   borderSide: BorderSide(color: Colors.black, width: 1),
+                  ),
+                  
+                ),
+
+                keyboardType: TextInputType.emailAddress, // Email keyboard
+
+              ),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                  ),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                   borderSide: BorderSide(color: Colors.black, width: 1),
+          ),
+
+                  suffixIcon: Icon(Icons.visibility),
+                  // onPressed: (){
+                  //   print('Toggle password visibility');
+                  // },
+                ),
+                obscureText: true, //to hide password input
+              )
+            ],
+          ),
+        ),
+          
         ]
+        
+
+      )
       )
     );
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
