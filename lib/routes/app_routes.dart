1f55157efdf25800b9d0f0c55b40a4929880f136
RuthMenber <ruthmenberdessie@gmail.com>
2025-02-12 00:07:22 +0300

    added settings and notification route

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:linkedin_flutter/main_screen.dart';
+import 'package:linkedin_flutter/screens/notification/notification_screen.dart';
 import 'package:linkedin_flutter/screens/search/search_screen.dart';
+import 'package:linkedin_flutter/screens/settings/settings_screen.dart';
 import '../screens/splash/splash_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
   static const String searchScreen = '/search';
   static const String mainScreen = '/main';
+  static const String settingScreen = '/settings';
+  static const String notificationScreen = '/notification';
   // static const String login = '/login';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -17,6 +21,10 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const Search());
       case mainScreen:
         return MaterialPageRoute(builder: (_) => const MainScreen());
+      case settingScreen:
+        return MaterialPageRoute(builder: (_) => const SettingsPage());
+      case notificationScreen:
+        return MaterialPageRoute(builder: (_) => const NotificationsScreen());
       // case login:
       //   return MaterialPageRoute(builder: (_) => const LoginPage());
       default:
