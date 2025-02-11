837629641d561c0ac582144bdc8147f387546163
RuthMenber <ruthmenberdessie@gmail.com>
2025-02-11 13:26:21 +0300

    addded routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:linkedin_flutter/main_screen.dart';
+import 'package:linkedin_flutter/screens/search/search_screen.dart';
 import '../screens/splash/splash_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
+  static const String searchScreen = '/search';
+  static const String mainScreen = '/main';
   // static const String login = '/login';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case splash:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case searchScreen:
+        return MaterialPageRoute(builder: (_) => const Search());
+      case mainScreen:
+        return MaterialPageRoute(builder: (_) => const MainScreen());
       // case login:
       //   return MaterialPageRoute(builder: (_) => const LoginPage());
       default:
